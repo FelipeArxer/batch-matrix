eafbef9e49b516782be782b2876a94ccfdda54ea
Felipe Arxer <felipearxer@outlook.com>
2024-02-07 12:56:44 -0300

    update new version

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # batch-matrix
-Batch script to create a matrix on console.
+Batch script to create "The Matrix" on your console. Just for fun!
+
+![Batch Matrix](https://github.com/FelipeArxer/batch-matrix/blob/master/images/batch-matrix.png?raw=true)
--- batch-matrix.bat
+++ batch-matrix.bat
@@ -9,14 +9,15 @@ cls
 :: 5 = Purple       D = Light Purple
 :: 6 = Yellow       E = Light Yellow
 :: 7 = White        F = Bright White
-:: Fisrt number [Background] Second number [Foreground]
-color 0A
+:: Fisrt is [Background] Second is [Foreground]
+color 02
 
 :: Use [%random%*] to generate a random integer number from 0 to 32767 (inclusive)
-:: Use [%random%*2/32767] to get random binary number (0 or 1)
+:: Use [%random%*2/32767] to get a random binary number (0 or 1)
 set ranbin=2/32767
 
 :matrix
+:: BLOCK 0
 set /a a0=%random%*%ranbin%
 set /a b0=%random%*%ranbin%
 set /a c0=%random%*%ranbin%
@@ -44,6 +45,48 @@ set /a x0=%random%*%ranbin%
 set /a y0=%random%*%ranbin%
 set /a z0=%random%*%ranbin%
 
-echo %a0% %b0% %c0% %d0% %e0% %f0% %g0% %h0% %i0% %j0% %k0% %l0% %m0% %n0% %o0% %p0% %q0% %r0% %s0% %t0% %u0% %v0% %w0% %x0% %y0% %z0% %a0% %b0% %c0% %d0% %e0% %f0% %g0% %h0% %i0% %j0% %k0% %l0% %m0% %n0% %o0% %p0% %q0% %r0% %s0% %t0% %u0% %v0% %w0% %x0% %y0% %z0% %a0% %b0% %c0% %d0% %e0% %f0% %g0% %h0%
+:: BLOCK 1
+set /a a1=%random%*%ranbin%
+set /a b1=%random%*%ranbin%
+set /a c1=%random%*%ranbin%
+set /a d1=%random%*%ranbin%
+set /a e1=%random%*%ranbin%
+set /a f1=%random%*%ranbin%
+set /a g1=%random%*%ranbin%
+set /a h1=%random%*%ranbin%
+set /a i1=%random%*%ranbin%
+set /a j1=%random%*%ranbin%
+set /a k1=%random%*%ranbin%
+set /a l1=%random%*%ranbin%
+set /a m1=%random%*%ranbin%
+set /a n1=%random%*%ranbin%
+set /a o1=%random%*%ranbin%
+set /a p1=%random%*%ranbin%
+set /a q1=%random%*%ranbin%
+set /a r1=%random%*%ranbin%
+set /a s1=%random%*%ranbin%
+set /a t1=%random%*%ranbin%
+set /a u1=%random%*%ranbin%
+set /a v1=%random%*%ranbin%
+set /a w1=%random%*%ranbin%
+set /a x1=%random%*%ranbin%
+set /a y1=%random%*%ranbin%
+set /a z1=%random%*%ranbin%
+
+:: BLOCK 2
+set /a a2=%random%*%ranbin%
+set /a b2=%random%*%ranbin%
+set /a c2=%random%*%ranbin%
+set /a d2=%random%*%ranbin%
+set /a e2=%random%*%ranbin%
+set /a f2=%random%*%ranbin%
+set /a g2=%random%*%ranbin%
+set /a h2=%random%*%ranbin%
+
+echo | set /p=" %a0% %b0% %c0% %d0% %e0% %f0% %g0% %h0% %i0% %j0% %k0% %l0% %m0% %n0% %o0% %p0% %q0% %r0% %s0% %t0% %u0% %v0% %w0% %x0% %y0% %z0% "
+echo | set /p=" %a1% %b1% %c1% %d1% %e1% %f1% %g1% %h1% %i1% %j1% %k1% %l1% %m1% %n1% %o1% %p1% %q1% %r1% %s1% %t1% %u1% %v1% %w1% %x1% %y1% %z1% "
+echo | set /p=" %a2% %b2% %c2% %d2% %e2% %f2% %g2% %h2% "
+
+::timeout /t 0 /nobreak > null
 
 goto matrix
